1dd7a25ac58da4b7ebfe54a3ede3a7e9389b5400
BoryanaZhelezova <boryana.zhelezova@gmail.com>
2017-01-28 23:36:59 +0200

    add folder

---
+++ CSSLayout/Music categories/LaptopStore.js
@@ -0,0 +1,15 @@
+        <script>
+            
+        function outputUpdateMinPrice(x) {
+            document.querySelector("#min-price").value = x;
+        }
+        function outputUpdateMaxPrice(x) {
+            document.querySelector("#max-price").value = x;
+        }
+        function outputUpdateMinRam(x) {
+            document.querySelector("#min-ram").value = x;
+        }
+        function outputUpdateMaxRam(x) {
+            document.querySelector("#max-ram").value = x;
+        }
+        </script>
